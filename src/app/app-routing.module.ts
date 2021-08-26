c04022b1d0b054f97ec4ae38ca9066fd0897d4a4
jimena <jimenahernando@hotmail.com>
2021-08-26 17:11:15 +0200

    Creo rutas,componentes y limpio el appComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListCharactersComponent } from './components/list-characters/list-characters.component';
+import { SingleCharacterComponent } from './components/single-character/single-character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListCharactersComponent },
+  { path: 'home', pathMatch: 'full', redirectTo: '/'},
+  { path: 'characters/', component: ListCharactersComponent },
+  { path: 'characters/:charId', component: SingleCharacterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCharactersComponent } from './components/list-characters/list-characters.component';
+import { ThumbnailCharacterComponent } from './components/thumbnail-character/thumbnail-character.component';
+import { SingleCharacterComponent } from './components/single-character/single-character.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCharactersComponent,
+    ThumbnailCharacterComponent,
+    SingleCharacterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
